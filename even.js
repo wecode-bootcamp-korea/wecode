934d07d2c06b2cfa43e9b0da90500dd1eec53326
Zaid Lee <zaidlee@Zaids-MacBook-Pro-2.local>
2021-03-05 14:01:44 +0900

    new: test.py

--- even.js
+++
@@ -1,4 +0,0 @@
-const is_even = () => {}
-
-console.log(is_even(11)) 
-console.log(is_even(10)) 
---
+++ test.py
@@ -0,0 +1,10 @@
+def even(numbers):
+    even = []
+    number = range(0,50)
+    if number % 2 == 0:
+        even.append(number)
+    
+    return even 
+
+
+    
